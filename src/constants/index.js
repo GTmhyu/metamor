89306342d7b70b9231d8aac2e0705ec52e0a5049
GTmhyu <zmaula10@gmail.com>
2026-07-08 15:33:32 +0700

    Product

--- src/constants/index.js
+++ src/constants/index.js
@@ -138,42 +138,39 @@ const techStackIcons = [
 
 const expCards = [
   {
-    review:
-      "Adrian brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
+    review: "Merupakan project TKDN yang sukses kami realisasikan berdasarkan imajinasi dan konsep yang dimiliki oleh client.",
     imgPath: "/images/exp1.png",
     logoPath: "/images/logo1.png",
-    title: "Frontend Developer",
-    date: "January 2023 - Present",
+    title: "Sculpture Park at Night",
+    date: "17 January 2026 ",
     responsibilities: [
-      "Developed and maintained user-facing features for the Hostinger website.",
-      "Collaborated closely with UI/UX designers to ensure seamless user experiences.",
-      "Optimized web applications for maximum speed and scalability.",
+      "Dengan LED flexible yang dapat mengikuti krangka struktur.",
+      "Bahan berkualitas yang membuatnya menjadi sangat baik untuk jangka panjang.",
+      "Desain arsitektur yang modern dan berseni yang membuat tampilan menjadi lebih exclusive.",
     ],
   },
   {
-    review:
-      "Adrian’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
+    review: "Belum tau",
     imgPath: "/images/exp2.png",
     logoPath: "/images/logo2.png",
-    title: "Full Stack Developer",
+    title: "Project",
     date: "June 2020 - December 2023",
     responsibilities: [
-      "Led the development of Docker's web applications, focusing on scalability.",
-      "Worked with backend engineers to integrate APIs seamlessly with the frontend.",
-      "Contributed to open-source projects that were used with the Docker ecosystem.",
+      "Dengan LED flexible yang dapat mengikuti krangka struktur.",
+      "Bahan berkualitas yang membuatnya menjadi sangat baik untuk jangka panjang.",
+      "Desain arsitektur yang modern dan berseni yang membuat tampilan menjadi lebih exclusive.",
     ],
   },
   {
-    review:
-      "Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
+    review: ".",
     imgPath: "/images/exp3.png",
     logoPath: "/images/logo3.png",
     title: "React Native Developer",
     date: "March 2019 - May 2020",
     responsibilities: [
-      "Built cross-platform mobile apps using React Native, integrating with Appwrite's backend services.",
-      "Improved app performance and user experience through code optimization and testing.",
-      "Coordinated with the product team to implement features based on feedback.",
+      "Dengan LED flexible yang dapat mengikuti krangka struktur.",
+      "Bahan berkualitas yang membuatnya menjadi sangat baik untuk jangka panjang.",
+      "Desain arsitektur yang modern dan berseni yang membuat tampilan menjadi lebih exclusive.",
     ],
   },
 ];
